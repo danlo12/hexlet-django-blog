c811880b78cbc8529ae0fdea9b70d4a67998e853
danlo <danlo12@mail.ru>
2024-01-09 17:31:46 +0300

    09.01

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import View
 
-def index(request):
-    return HttpResponse('article')
+class Index(View):
+
+    template_name = "articles.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tags'] = 'World'
+        return context
 # Create your views here.
