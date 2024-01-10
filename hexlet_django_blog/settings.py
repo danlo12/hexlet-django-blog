f960e0cf096fe93d915c0d861c1e40a69536b860
danlo <danlo12@mail.ru>
2024-01-10 22:04:36 +0300

    add config and admin

--- hexlet_django_blog/settings.py
+++ hexlet_django_blog/settings.py
@@ -9,21 +9,26 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from dotenv import load_dotenv
 from pathlib import Path
+import os
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG', False)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3xw=m#!r7v=j5b1k&-gzb4-p+23m_^119!cr$zidyqw1_q*m%l'
+#SECRET_KEY = 'django-insecure-3xw=m#!r7v=j5b1k&-gzb4-p+23m_^119!cr$zidyqw1_q*m%l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
 ALLOWED_HOSTS = []
 
