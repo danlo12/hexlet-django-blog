fa6e142c9f823ffb1f3356e9286ec2d96e533fec
danlo <danlo12@mail.ru>
2024-03-01 22:59:55 +0300

    1

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from hexlet_django_blog.article.models import Article
 from django.shortcuts import render, get_object_or_404
+from django.forms import ModelForm
 class IndexView(View):
 
     def get(self, request, *args, **kwargs):
@@ -19,5 +20,22 @@ class ArticleView(View):
         return render(request, 'articles/show.html', context={
             'article': article,
         })
+
+class ArticleForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ["name","body"]
+
+class ArticleFormCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'articles/create.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/articles')
+        return render(request, 'articles/create.html', {'form': form})
 # Create your views here.
 
